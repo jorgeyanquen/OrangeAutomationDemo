245c75ec8b639bd42ad2a022f680e38f7ee7c9f2
jorgeyanquen <jorgeyanquen63@gmail.com>
2024-08-09 09:12:47 -0500

    version 2 orange

--- src/test/java/com/screenplay/practicados/Steps/LoginOrangeStep.java
+++ src/test/java/com/screenplay/practicados/Steps/LoginOrangeStep.java
@@ -1,12 +1,17 @@
 package com.screenplay.practicados.Steps;
 
 import com.screenplay.practicados.pages.LoginOrangePage;
+import net.thucydides.core.annotations.Step;
 
 public class LoginOrangeStep {
 
     LoginOrangePage loginOrangePage;
 
+    @Step
     public void login(){
         loginOrangePage.open();
+        loginOrangePage.loginusername();
+        loginOrangePage.loginpassword();
+        loginOrangePage.botonlogin();
     }
 }
--- src/test/java/com/screenplay/practicados/pages/LoginOrangePage.java
+++ src/test/java/com/screenplay/practicados/pages/LoginOrangePage.java
@@ -1,10 +1,36 @@
 package com.screenplay.practicados.pages;
 
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 @DefaultUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 public class LoginOrangePage extends PageObject {
+    @FindBy(name = "username")
+    public WebElementFacade txtusername;
 
+    @FindBy(name = "password")
+    public WebElementFacade txtpassword;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")
+    public WebElementFacade btnlogin;
+
+    public void loginusername(){
+        txtusername.click();
+        txtusername.clear();
+        txtusername.sendKeys("admin");
+    }
+
+    public void loginpassword(){
+        txtpassword.click();
+        txtpassword.clear();
+        txtpassword.sendKeys("admin123");
+    }
+
+    public void botonlogin(){
+        btnlogin.click();
+    }
 
 }
